d9427bb3630374dde69f90346d4456ab519fcdd0
Josh <j.maxwell@mantel.ae>
2021-10-18 14:47:03 -0400

    style uploads form

--- shared/index.php
+++ shared/index.php
@@ -34,15 +34,35 @@
             <html>
                 <body>
 
+                    <style>
+                        .controlpanel {background: white;width: 600px;max-width: 100%;margin: auto;padding: 26px;border-radius: 9px;border-top: 7px solid #85a5e6;box-shadow: 0 20px 40px -35px;margin-top: 20vh;position: relative;}
+                        body {background: #e6e7ea;font-family: arial;}
+                        form{position: relative;border-bottom: 1px solid;padding-bottom: 10px;}
+                        .radio label {position: absolute;margin: -25px 36px;}
+                        .radio input {display: block;margin: 10px;}
+                        input[type="submit"] {position: absolute;right: 0px;bottom: 20px;}
+                        select {padding: 5px 10px;margin: 10px;width: 200px;}
+                        .results {width: 50%;display: inline-block;margin: 0 10px;}
+                        .linkarea {right: 30px;position: absolute;display: inline-block;text-align: right;}
+                        .linkarea a {margin: 5px;}
+                        .results input {border: none;}
+                        input[type="file"] {position: absolute;right: 10px;top: 20px;width: 310px;border-right: 1px solid black;}
+                        ::file-selector-button, input[type="submit"] {background: #4875d2;border: none;padding: 7px 20px;color: white;margin: 0 10px;cursor: pointer;}
+                        ::file-selector-button:hover, input[type="submit"]:hover {background: #2d56ac;}
+                        [data-mode="template"] input[type="file"] {display: none;}
+                        [data-mode="template"] textarea {display: block;position: absolute;right: 10px;top: 0px;width: 310px;height: 70px;}
+                        textarea {display: none;}
+                    </style>
+
                     <div class="controlpanel">
 
                         <form id="form" name="form" method="post" action="index.php" enctype="multipart/form-data" onsubmit="submit(event)">
                             <input type="hidden" name="hc" value="<?php echo $_GET["hc"]; ?>" />
                             <div class="radio">
-                                <input type="radio" id="img" name="type" value="img"><label for="img">Image</label>
-                                <input type="radio" id="vid" name="type" value="vid"><label for="vid">Video</label>
-                                <input type="radio" id="file" name="type" value="file"><label for="file">File</label>
-                                <input type="radio" id="template" name="type" value="template"><label for="template">HTML Template</label>
+                                <input onchange="radio(this)" checked type="radio" id="img" name="type" value="img"><label for="img">Image</label>
+                                <input onchange="radio(this)" type="radio" id="vid" name="type" value="vid"><label for="vid">Video</label>
+                                <input onchange="radio(this)" type="radio" id="file" name="type" value="file"><label for="file">File</label>
+                                <input onchange="radio(this)" type="radio" id="template" name="type" value="template"><label for="template">HTML Template</label>
                             </div>
                             <select name="dir">
                                 <?php foreach( scandir( __DIR__ . "/images/" ) as $dir ) if( $dir !== "." && $dir !== ".." && $dir !== "index.php" ) echo "<option value='$dir'>$dir</option>"; ?>
@@ -53,7 +73,7 @@
                         </form>
 
                         <div class="results">
-                            <label>Reponse</label>
+                            <label>Response</label>
                             <input placeholder="..." id="resulta">
                         </div>
 
@@ -65,6 +85,10 @@
                         </div>
 
                         <script>
+                            function radio(e){
+                                e.parentNode.parentNode.dataset.mode = e.id;
+                            }
+
                             async function submit(event) {	
                                 event.preventDefault();
                                 document.getElementById("form").dataset.loading = "true";
