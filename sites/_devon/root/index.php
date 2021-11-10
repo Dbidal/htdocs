8650c3376f7713dde872f88a3666d3fd7c0c9048
Dbidal^ <dbidal@dentalgrowthstrategies.com>
2021-11-10 11:20:10 -0500

    test changes

---
+++ sites/_devon/root/index.php
@@ -0,0 +1,16 @@
+<?php include preg_replace( "/(sites).*/", "", __DIR__ ) . "head.php"; ?>
+
+<!-- top bar -->
+<style>
+		@media(max-width:700px) {.head:not(#_) figure {display: none;}}
+	</style>
+
+	<div class="wp-block-group head" style="background:#20202f;position:sticky;top:0;z-index:4;max-width: 100%;padding:10px;">
+		<div class="wp-block-group" style="max-width:500px;margin: 10px auto;">
+			<figure class="wp-block-image alignleft" style="margin: 0;">
+				<?php new Image( "https://htdocs.dgstesting.com/shared/images/giorgio/highres/9105738.png", "", array( "width" => 62, "height" => 63 ) ); ?>
+			</figure>
+			<p style="color:#ffffff;font-size:24px;margin:5px 0px 0px"><b>GIORGIO CONSTRUCTION&nbsp;INC.</b></p>
+			<p style="color:#ffffff;margin:0;">SPECIALIZING IN WATERPROOFING</p>
+		</div> 
+	</div>
